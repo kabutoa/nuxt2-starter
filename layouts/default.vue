f59ae3b9e0b0f9d584835ee3544d037aad4926e5
kabutoa <yinyun957@163.com>
2022-11-08 07:09:04 +0800

    feat: init layouts

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="app">
+    <global-header />
+    <nuxt keep-alive />
+  </div>
+</template>
+
+<script>
+import GlobalHeader from '@/components/header'
+
+export default {
+  components: {
+    GlobalHeader,
+  },
+}
+</script>
---
+++ layouts/error.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="app-error">
+    <h1 v-if="error.statusCode === 404">Page Not Found</h1>
+    <h1 v-else>Page Error</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['error'],
+}
+</script>
