2660cbc0464ac08bfaac5f2fb37e3b571c8f55e3
kabutoa <yinyun957@163.com>
2022-11-09 15:02:29 +0800

    feat: update nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,28 +1,38 @@
+import * as cheerio from 'cheerio'
+
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'nuxt2-starter',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+
+  loading: {
+    color: '#4FC08D',
+    failedColor: '#bf5050',
+    duration: 1500,
+  },
+
+  // https://nuxtjs.org/docs/configuration-glossary/configuration-server
+  server: {
+    port: 8080,
+    host: '0.0.0.0',
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: ['normalize.css/normalize.css', '@/assets/css/index.scss'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: ['@/plugins/axios'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -40,9 +50,26 @@ export default {
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {},
+  axios: {
+    // baseUrl: '/',
+    // prefix: '/api',
+    // proxy: true,
+  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-  }
+    styleResources: {
+      scss: ['@/assets/css/variable.scss', '@/assets/css/mixin.scss'],
+    },
+    // 防止重复大包
+    vendor: ['axios'],
+  },
+
+  hooks: {
+    // 页面渲染后返回之前
+    'render:route': (url, result, { req }) => {
+      const $ = cheerio.load(result.html)
+      result.html = $.html()
+    },
+  },
 }
